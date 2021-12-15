000da4934c47cae18825d4848e31349fd5f23416
Eleazar Nascimento <eleazar.nascimento@gmail.com>
2021-12-14 23:11:49 -0300

    feat: cria componente SubscribeButton

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
 import Head from "next/head";
+import { SubscribeButton } from "../components/SubscribeButton";
 
-import styles from './home.module.scss';
+import styles from "./home.module.scss";
 
 export default function Home() {
   return (
@@ -11,11 +12,14 @@ export default function Home() {
       <main className={styles.contentContainer}>
         <section className={styles.hero}>
           <span> 👏 Hey, welcome</span>
-          <h1>News about the <span>React</span> world.</h1>
+          <h1>
+            News about the <span>React</span> world.
+          </h1>
           <p>
             Get access to all the published <br />
             <span>for $9.99 month</span>
           </p>
+          <SubscribeButton />
         </section>
 
         <img src="/images/avatar.svg" alt="Girl coding" />
